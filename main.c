6ff830b370731b6cc4fabb09be00d9a7a4ac42bc
qionghuang6 <qhuang00@stuy.edu>
2019-12-04 11:44:41 -0500

    use strtok to get rid of \n

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "parse.h"
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
   char string[128];
   fgets(string, 128, stdin);
+  strtok(string, "\n");
   printf("entered: %s\n", string);
   char ** args = parse_args(string);
   for (size_t i = 0; args[i] != NULL; i++) {
     printf("%s ", args[i]);
   }
   printf("\n");
+  execvp(args[0], args);
   return 0;
 }
